d710ba9dbacaf4540db2aab1a00acfe9fe0463c4
OrdinarilySam <56322367+OrdinarilySam@users.noreply.github.com>
2023-06-28 17:26:25 -0500

    added shortest and longest games

--- src/Simulate.java
+++ src/Simulate.java
@@ -8,12 +8,20 @@ public class Simulate {
         long roundsSum = 0L;
         long warsSum = 0L;
         long reshufflesSum = 0L;
+
+        int longestGame = 0;
+        int shortestGame = Integer.MAX_VALUE;
         Game game = new Game();
         for(int i = 0; i < rounds; i++){
             int[] result = game.startGame();
             roundsSum += result[0];
             warsSum += result[1];
             reshufflesSum += result[2];
+            int timeInSec = (result[0] * secondsInRound)
+                    + (result[1] * secondsInWar)
+                    + (result[2] * secondsInReshuffle);
+            longestGame = Math.max(longestGame, timeInSec);
+            shortestGame = Math.min(shortestGame, timeInSec);
         }
 
         int avgRounds = (int) roundsSum/rounds;
@@ -27,6 +35,9 @@ public class Simulate {
         System.out.println("Average number of rounds: " + avgRounds);
         System.out.println("Average number of wars: " + avgWars);
         System.out.println("Average number of reshuffles: " + avgReshuffles);
+        System.out.println();
         System.out.println("Average time spent per round: " + (float) timeSpent / 60 + " minutes");
+        System.out.println("Longest game: " + (float) longestGame / 60 + " minutes");
+        System.out.println("Shortest game: " + (float) shortestGame / 60 + " minutes");
     }
 }
